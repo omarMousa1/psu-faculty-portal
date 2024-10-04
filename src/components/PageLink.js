bf295811e5c210ab48361692ba6d6c36a1e22444
Omar Mousa <86836557+omarMousa1@users.noreply.github.com>
2024-10-04 12:41:53 +0300

    Add files via upload

--- src/components/PageLink.js
+++ src/components/PageLink.js
@@ -7,7 +7,7 @@ export const PageLink = () => {
         <li>
           <a
             href="http://#"
-            className="lg:px-1 lg:py-[1px] md:px-1 md:py-[1px] px-1 py-[2px] border rounded-l-md c flex items-center text-base text-[#777] border-[#ddd]"
+            className="lg:px-3 lg:py-[8px] md:px-3 md:py-[8px] px-3 py-[8px] border rounded-l-md text-base text-[#777] border-[#ddd]"
           >
             Previous
           </a>
@@ -17,7 +17,7 @@ export const PageLink = () => {
           <li key={page}>
             <a
               href="http://#"
-              className={`lg:px-3 lg:pb-[4px] lg:pt-[4px] md:px-3 md:pb-[4px] md:pt-[4px] px-3 pb-[5px] pt-[4px] border border-[#ddd] hover:bg-gray-200 ${
+              className={`lg:px-3 lg:py-[8px] md:px-3 md:py-[8px] px-3 py-[8px] border border-[#ddd] hover:bg-gray-200 ${
                 page === 1
                   ? "bg-[#337ab7] text-white border-[#337ab7]"
                   : "bg-white text-[#337ab7]"
@@ -31,7 +31,7 @@ export const PageLink = () => {
         <li>
           <a
             href="http://#"
-            className="lg:px-1 lg:py-[1px] md:px-1 md:py-[1px] px-1 py-[2px] border rounded-r-md text- flex items-center text-[#337ab7] text-base border-[#ddd]"
+            className="lg:px-3 lg:py-[8px] md:px-3 md:py-[8px] px-3 py-[8px] border rounded-r-md text-[#337ab7] text-base border-[#ddd]"
           >
             Next
           </a>
